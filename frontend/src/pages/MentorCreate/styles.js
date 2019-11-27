7ae9af9c81b2b6e2c22f62ffae74f59581154079
Rogerio <rogerio.francis@gmail.com>
2019-11-27 00:17:40 -0300

    Add MentorCreate

---
+++ frontend/src/pages/MentorCreate/styles.js
@@ -0,0 +1,42 @@
+import styled from "styled-components"
+
+export const Container = styled.div`
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  form {
+    width: 100%;
+    max-width: 300px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+
+    span {
+      align-self: center;
+    }
+
+    input {
+      margin-top: 20px;
+      border: 1px solid #ddd;
+      border-radius: 4px;
+      height: 48px;
+      padding: 0 20px;
+      font-size: 16px;
+      color: #666;
+    }
+
+    button {
+      margin-top: 10px;
+      border: 0;
+      border-radius: 4px;
+      height: 48px;
+      font-size: 16px;
+      background: #5d8cf6;
+      font-weight: bold;
+      color: #fff;
+      cursor: pointer;
+    }
+  }
+`
